10ef70595e13fd85b4f63cb17b5c2095bbf6c517
shimo8810 <shimo.8810@gmail.com>
2024-09-10 21:31:12 +0900

    feat: change message

--- src/notes.tsx
+++ src/notes.tsx
@@ -3,6 +3,6 @@ export const title = "Notes";
 export default (data: Lume.Data, _helpers: Lume.Helpers) => (
   <>
     <h1>{data.title}</h1>
-    <p>This is my first post using lume. I hope you like it!</p>
+    <h2>under construction</h2>
   </>
 );
