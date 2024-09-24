c10f106c069b7f2780c95bcc3fdf0237ceebd31c
shimo8810 <shimo.8810@gmail.com>
2024-09-24 22:18:50 +0900

    feat: use tsx from md for home

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+export const title = "shimo8810.dev";
+
+export default (_data: Lume.Data, _helpers: Lume.Helpers) => {
+  return (
+    <article>
+      <h2>ここは何?</h2>
+      ここは私(shimo8810)が技術系の記事やつぶやきを書くためのページ
+      <h2>分野</h2>
+      Python, Rust, Nix, k8s, MacOS, deno, Raspberry Pi, Arduino, etc.
+    </article>
+  );
+};
