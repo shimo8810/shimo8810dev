df692cce30e5fb000c7897bd3926fa1bcf50fe50
shimo8810 <shimo.8810@gmail.com>
2024-09-24 22:16:51 +0900

    fix: add key for sort

--- src/posts.tsx
+++ src/posts.tsx
@@ -4,7 +4,7 @@ export default (data: Lume.Data, _helpers: Lume.Helpers) => {
   return (
     <div className="post-list">
       {data.search.pages("post", "date=desc").map((post) => (
-        <div className="post-link">
+        <div key={post.date.toLocaleDateString("sv-SE")} className="post-link">
           <a href={post.url}>{post.title}</a>
           <span>{post.date.toLocaleDateString("sv-SE")}</span>
         </div>
