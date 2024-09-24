a41f4d7fc8a995cc097af9e14b0c6cc932972657
shimo8810 <shimo.8810@gmail.com>
2024-09-24 22:19:06 +0900

    feat: use list for links

--- src/_includes/templates/header.tsx
+++ src/_includes/templates/header.tsx
@@ -1,10 +1,22 @@
 export default () => {
+  const links = [
+    { href: "/", label: "home" },
+    { href: "/posts", label: "posts" },
+    { href: "/notes", label: "notes" },
+    { href: "/about", label: "about" },
+  ];
+
   return (
-    <header>
-      <a href="/">home</a>
-      <a href="/posts">posts</a>
-      <a href="/notes">notes</a>
-      <a href="/about">about</a>
+    <header className="p-2 m-0 bg-nord6 border-b-2 border-nord4">
+      {links.map((link, index) => (
+        <a
+          key={index}
+          href={link.href}
+          className="text-lg my-0 mx-2 py-0.5 px-2 rounded text-nord0 no-underline hover:bg-nord4"
+        >
+          {link.label}
+        </a>
+      ))}
     </header>
   );
 };
